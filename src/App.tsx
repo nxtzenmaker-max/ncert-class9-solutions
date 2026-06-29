6fce3e9e317083b05b8b8bb983cf6f624c701137
zeno191gt6-stack <zeno191gt6@gmail.com>
2026-06-30 01:01:42 +0530

    Fix routing - remove WouterRouter wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Router as WouterRouter } from "wouter";
+import { Switch, Route } from "wouter";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,25 +10,17 @@ import Loader from "@/components/Loader";
 
 const queryClient = new QueryClient();
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/subject/:id" component={Subject} />
-      <Route path="/chapter/:id" component={ChapterPage} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Loader />
-        <WouterRouter>
-          <Router />
-        </WouterRouter>
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/subject/:id" component={Subject} />
+          <Route path="/chapter/:id" component={ChapterPage} />
+          <Route component={NotFound} />
+        </Switch>
         <Toaster />
       </TooltipProvider>
     </QueryClientProvider>

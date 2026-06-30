096dd34de1b8170f302fa1d6957b37d85bb2aa45
zeno191gt6-stack <zeno191gt6@gmail.com>
2026-06-30 06:51:07 +0530

    Add error display in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
 import "./index.css";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root");
+if (!rootEl) {
+  document.body.innerHTML = '<h1 style="color:red">ROOT NOT FOUND</h1>';
+} else {
+  try {
+    createRoot(rootEl).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  } catch (e: any) {
+    rootEl.innerHTML = '<div style="padding:20px;color:red;font-family:monospace"><h2>REACT ERROR</h2><pre>' + e.message + '\n' + e.stack + '</pre></div>';
+  }
+}
